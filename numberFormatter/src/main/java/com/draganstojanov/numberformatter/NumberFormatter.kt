0f495728ca9c7999769a6ea63c681108a33a3982
dragan.stojanov <dragan.stojanov@gmail.com>
2022-04-08 17:03:28 +0200

    example - layout part

--- numberFormatter/src/main/java/com/draganstojanov/numberformatter/NumberFormatter.kt
+++ numberFormatter/src/main/java/com/draganstojanov/numberformatter/NumberFormatter.kt
@@ -4,14 +4,14 @@ import com.draganstojanov.numberformatter.util.ShowDecimals
 
 
 class NumberFormatter(
-    val leadingZeros: Int = 0,
+    var leadingZeros: Int = 0,
     val showDecimals: ShowDecimals = ShowDecimals.DEFAULT,
     val showIntIfZero: Boolean = true,
     val fixedDecimals: Int = 0,
     val maxDecimals: Int = 0
 ) {
 
-    fun formatted(number: Number): String = Formatter.formatter(
+    fun getFormatted(number: Number?): String = Formatter.formatter(
         number = number,
         leadingZeros = leadingZeros,
         showDecimals = showDecimals,
