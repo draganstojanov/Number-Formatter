469585ba4c2d4fcf9a29a66e1a499cb2143f2053
dragan.stojanov <dragan.stojanov@gmail.com>
2022-04-11 12:19:45 +0200

    maxDecimals refactor

--- numberFormatter/src/main/java/com/draganstojanov/numberformatter/NumberFormatter.kt
+++ numberFormatter/src/main/java/com/draganstojanov/numberformatter/NumberFormatter.kt
@@ -7,8 +7,8 @@ class NumberFormatter(
     var leadingZeros: Int = 0,
     var showDecimals: ShowDecimals = ShowDecimals.DEFAULT,
     var showIntIfZero: Boolean = true,
-    var fixedDecimals: Int = 0,
-    var maxDecimals: Int = 0
+    var maxDecimals: Int = 0,
+    var addZerosAtEnd: Boolean = false
 ) {
 
     fun getFormatted(number: Number?): String = Formatter.formatter(
@@ -16,8 +16,8 @@ class NumberFormatter(
         leadingZeros = leadingZeros,
         showDecimals = showDecimals,
         showIntIfZero = showIntIfZero,
-        fixedDecimals = fixedDecimals,
-        maxDecimals = maxDecimals
+        maxDecimals = maxDecimals,
+        addZerosAtEnd = addZerosAtEnd
     )
 
 }
