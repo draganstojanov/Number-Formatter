b037914e3c6a7a80f7e964593dfeedefc37e0cee
dragan.stojanov <dragan.stojanov@gmail.com>
2022-04-13 00:45:00 +0200

    bugfix

--- numberFormatter/src/main/java/com/draganstojanov/numberformatter/ext/NumberFormatterExtensions.kt
+++ numberFormatter/src/main/java/com/draganstojanov/numberformatter/ext/NumberFormatterExtensions.kt
@@ -7,10 +7,9 @@ import com.draganstojanov.numberformatter.util.ShowDecimals
 
 fun Int.addSingleLeadingZero(): String = if (this.toString().length <= 2) "0${this}".takeLast(2) else this.toString()
 
-//fun Number.addLeadingZeros(leadingZeros: Int): String = formatter(this, leadingZeros = leadingZeros)
-fun Number.addLeadingZeros(leadingZeros: Int): String = Formatter.addLeadingZeros(number = this, zeros = leadingZeros)
+fun Number.addLeadingZeros(leadingZeros: Int): String = formatter(this, leadingZeros = leadingZeros)
 
-fun Number.showDecimals(showDecimals: ShowDecimals): String = formatter(this, showDecimals = showDecimals)
+fun Number.showDecimals(showDecimals: ShowDecimals): String = formatter(this, showDecimals = showDecimals)//TODO
 
 fun Number.showIntegerPartIfZero(showIntIfZero: Boolean): String = formatter(this, showIntIfZero = showIntIfZero)
 
--- numberFormatter/src/main/java/com/draganstojanov/numberformatter/util/Const.kt
+++
@@ -1,3 +0,0 @@
-package com.draganstojanov.numberformatter.util
-
-//internal const val NO_DECIMAL_VALUE = -1
